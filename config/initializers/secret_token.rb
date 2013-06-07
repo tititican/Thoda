b6f5cd9c77d05a4ae4c25f4627d4e49e5e34b1aa
An D. Hoang <an@izproperty.com>
2013-06-07 15:13:27 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbboApp::Application.config.secret_token = 'c1b14c44b1b9261d473123515c09be720fa22affaf8482997064e59548095b826cab98da811912a37dccfc6550e07b5379b23c0ea3bd8fab3ad39e09a3eb3858'
