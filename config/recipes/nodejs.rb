89450129103f351f5d40d0a2d6c5f28b57c7adae
An D. Hoang <an@izproperty.com>
2013-07-13 23:16:01 +0700

    Initial setup

--- config/recipes/nodejs.rb
+++
@@ -1,7 +0,0 @@
-namespace :nodejs do
-  desc "Install the latest relase of Node.js"
-  task :install, roles: :app do
-    run "#{sudo} apt-get -y install nodejs"
-  end
-  after "deploy:install", "nodejs:install"
-end
