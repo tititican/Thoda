dc14fdc6e1db1f2f6e57c71c5c62993ed769bf79
An D. Hoang <an@izproperty.com>
2013-07-13 13:49:11 +0700

    fix assets in production mode

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -16,7 +16,6 @@
 //= require waypoints.min
 //= require bootstrap
 //= require galleria-1.2.9.min
-//= require galleria.twelve.min
 //=	require jquery.isotope.min
 //= require retina
 //= require project_custom
