dc38d3b08ebaeb435df5183d145d96d8411dbc94
baont <baont@railsdev.(none)>
2014-02-27 15:31:26 +0700

    Setting Server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "rvm/capistrano"
 require "bundler/capistrano"
 
-server "162.243.240.224", :web, :app, :db, primary: true
+server "128.199.228.94", :web, :app, :db, primary: true
 
 set :application, "abbo"
 set :user, "deployer"
